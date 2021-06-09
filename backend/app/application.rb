83c8758c884364eebfb94f634c2e1721817bf896
joeljdv <joeldiazjdv@gmail.com>
2021-06-09 19:39:49 -0400

    create path to directors/id

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -4,12 +4,17 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    binding.pry
+    # binding.pry
     if req.path.match(/test/) 
       return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
     elsif req.path.match(/directors/)
-      directors = Director.all
-      return [200, { 'Content-Type' => 'application/json' }, [directors.to_json ]]
+      if req.path.split('/directors').length == 0
+        directors = Director.all
+        return [200, { 'Content-Type' => 'application/json' }, [directors.to_json ]]
+      else
+        director_id = req.path.split('/directors/').last
+        return [200, { 'Content-Type' => 'application/json' }, [Director.find_by(id: director_id).to_json({:include => :movies}) ]]
+      end
     else
       resp.write "Path Not Found"
 
