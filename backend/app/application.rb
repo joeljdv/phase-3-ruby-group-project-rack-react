01f0ae91193e89f0202a8079404672a5e11bc005
joeljdv <joeldiazjdv@gmail.com>
2021-06-09 19:19:58 -0400

    create path for directors

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -4,9 +4,12 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
+    binding.pry
     if req.path.match(/test/) 
       return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
-
+    elsif req.path.match(/directors/)
+      directors = Director.all
+      return [200, { 'Content-Type' => 'application/json' }, [directors.to_json ]]
     else
       resp.write "Path Not Found"
 
