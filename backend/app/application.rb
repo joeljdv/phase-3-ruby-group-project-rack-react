0d3f178c0e0e2be719c08c3d318269d6bb36fb61
joeljdv <joeldiazjdv@gmail.com>
2021-06-10 16:37:56 -0400

    create path to post a directors movie

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -7,17 +7,23 @@ class Application
     # binding.pry
     if req.path.match(/test/) 
       return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
+    elsif req.path.match(/movies/)
+      input = JSON.parse(req.body.read)
+      director_id = req.path.split('/directors/').last.split("/movies").last
+      director = Director.find_by(id: director_id)
+      movie = director.movies.create(name: input["name"])
+      return [200, { 'Content-Type' => 'application/json' }, [movie.to_json ]]
     elsif req.path.match(/directors/)
       if req.env["REQUEST_METHOD"] == "POST"
         input = JSON.parse(req.body.read)
         director = Director.create(name: input["name"])
-        return [200, { 'Content-Type' => 'application/json' }, [directors.to_json ]]
+        return [200, { 'Content-Type' => 'application/json' }, [director.to_json ]]
       else 
         if req.path.split('/directors').length == 0
-        return [200, { 'Content-Type' => 'application/json' }, [Director.all.to_json ]]
-      else
-        director_id = req.path.split('/directors/').last
-        return [200, { 'Content-Type' => 'application/json' }, [Director.find_by(id: director_id).to_json({:include => :movies}) ]]
+          return [200, { 'Content-Type' => 'application/json' }, [Director.all.to_json ]]
+        else
+          director_id = req.path.split('/directors/').last
+          return [200, { 'Content-Type' => 'application/json' }, [Director.find_by(id: director_id).to_json({:include => :movies}) ]]
         end 
       end
     else
