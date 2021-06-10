179138f4c9afebf91f55dc16194f89f405220c51
joeljdv <joeldiazjdv@gmail.com>
2021-06-10 15:12:13 -0400

    create new path to POST a new director

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -8,12 +8,17 @@ class Application
     if req.path.match(/test/) 
       return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
     elsif req.path.match(/directors/)
-      if req.path.split('/directors').length == 0
-        directors = Director.all
+      if req.env["REQUEST_METHOD"] == "POST"
+        input = JSON.parse(req.body.read)
+        director = Director.create(name: input["name"])
         return [200, { 'Content-Type' => 'application/json' }, [directors.to_json ]]
+      else 
+        if req.path.split('/directors').length == 0
+        return [200, { 'Content-Type' => 'application/json' }, [Director.all.to_json ]]
       else
         director_id = req.path.split('/directors/').last
         return [200, { 'Content-Type' => 'application/json' }, [Director.find_by(id: director_id).to_json({:include => :movies}) ]]
+        end 
       end
     else
       resp.write "Path Not Found"
